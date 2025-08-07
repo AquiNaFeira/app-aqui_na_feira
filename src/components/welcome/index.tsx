0cd8d2d8e70858020b60d3e0a129a1f90aaad342
Poliana Sidere <poli360-pp@hotmail.com>
2025-08-07 11:07:11 -0300

    feat: add routes ao projeto

--- src/components/welcome/index.tsx
+++ src/components/welcome/index.tsx
@@ -1,8 +1,15 @@
-import { Image, Text, View } from "react-native"
+import React from "react";
+import { Image, Text, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { StackNavigatorProps } from '@react-navigation/stack';
 
 import { s } from "./style"
 
-export function Welcome() {
+type WelcomeScreenNavigationProp = StackNavigatorProps<RootStackParamList, 'Welcome'>;
+
+export default function Welcome() {
+  const navigator = useNavigation<WelcomeScreenNavigationProp>();
+
   return (
     <View>
       <Image source={require("@/assets/logo.png")} style={s.logo} />
@@ -10,12 +17,13 @@ export function Welcome() {
       <Text style={s.title}>Bem-Vindo ao Aqui na Feira!</Text>
 
       <Text style={s.subtitle}>
-        Explore a Feira de Caruaru e encontre {"\n"} 
+        Explore a Feira de Caruaru e encontre 
         tudo que procura. {"\n"}
-        Planeje a melhor rota para encontrar {"\n"}
+        Planeje a melhor rota para encontrar
         feirantes, bancos e toda variedade {"\n"}
         que a feira oferece!
       </Text>
+      
       <button
       title="Entrar"
       onPress={() => navigator.navigate("/login")}
