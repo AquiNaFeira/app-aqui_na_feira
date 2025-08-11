2de425d4a9746d5b469f6bca9194fc1c156092f4
Poliana Sidere <poli360-pp@hotmail.com>
2025-08-11 15:17:28 -0300

    feat: add auth.Provider e ProtectedRoute

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -32,8 +32,7 @@ export default function Layout(){
                 <AuthProvider>
                     <Stack>
                         <Stack.Screen name="index" options={{ headerShown: false }} />
-                        <Stack.Screen name="register" options={{ headerShown: false }} />
-                        <Stack.Screen name="login" options={{ headerShown: false }} />
+                        <Stack.Screen name="( auth )" options={{ headerShown: false }} />
                         <Stack.Screen name="( tabs )" options={{ headerShown: false }} />
                     </Stack>
                 </AuthProvider>
--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,12 +1,14 @@
-import React, { createContext, useContext, useState, ReactNode } from "react";
+import React, { createContext, useContext, useState, ReactNode, useEffect } from "react";
+import { useSegments, useRouter } from "expo-router";
+import { s } from "@/components/bancoBottomSheet/style";
 
 type User = {
   id: string;
   email: string;
-} | null;
+}
 
 type AuthContextType = {
-  user: User;
+  user: User | null;
   token: string | null;
   loading: boolean;
   login: (email: string, password: string) => Promise<void>;
@@ -16,18 +18,61 @@ type AuthContextType = {
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const [user, setUser] = useState<User>(null);
+  const [user, setUser] = useState<User | null>(null);
   const [token, setToken] = useState<string | null>(null);
   const [loading, setLoading] = useState(true);
+  const segments = useSegments();
+  const router = useRouter();
 
   const login = async (email: string, password: string) => {
+    setLoading(true);
     console.log("Tocando a campainha...");
+    
+    await new Promise(resolve => setTimeout(resolve, 1500)); 
+    
+    const fakeToken = "abc-123-def-456";
+    const fakeUser = { id: "user-123", email: email };
+    
+    setUser(fakeUser);
+    setToken(fakeToken);
+    
+    setLoading(false);
   };
 
   const logout = async () => {
+    setLoading(true);
+    console.log("Saindo...");
+
+    await new Promise(resolve => setTimeout(resolve, 500));
+
     setUser(null);
     setToken(null);
+    setLoading(false);
   };
+
+  useEffect(() => {
+    if (!loading) {
+      const isSignedIn = !!user;
+      const isAuthRoute = segments[0] === "( auth )";
+
+      if (isSignedIn && isAuthRoute) {
+        router.replace("/( tabs )");
+      } else if (!isSignedIn && !isAuthRoute) {
+        router.replace("/( auth )/login");
+      }
+    }
+  } , [loading, user, segments]);
+
+  useEffect (() => {
+    const checkInitialAuth = async () => {
+      await new Promise(resolve => setTimeout(resolve, 1000));
+
+      setUser(null);
+      setToken(null);
+      setLoading(false);
+    };
+    checkInitialAuth();
+  }, []);
 
   return (
     <AuthContext.Provider value={{ user, token, loading, login, logout }}>
